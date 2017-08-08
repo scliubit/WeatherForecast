0c8c6e3c7b03e284ab7bdb2dc9cae5fa9ffde4e2
Psycho <13513034769@163.com>
2017-08-08 23:47:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 import socket
 import re
 from Alert import Alert as Alt
+import Alert
 from urllib import request
 from urllib.parse import quote
 AlertObj = Alt()
@@ -14,7 +15,9 @@ runtime = time.time()
 class WeatherInfo(object):
     def __init__(self, weather_dict):
         if __name__ == '__main__':
-            AlertObj.Info("Constructing Weather Info Module")
+            AlertObj.Info(
+                "Constructing Weather Info Module",
+                color=Alert.FOREGROUND_WHITE)
         if weather_dict['HeWeather5'][0]['status'] == 'ok':
             pass
         else:
@@ -96,12 +99,15 @@ class WeatherInfo(object):
         self.now_humidity = self.now['hum']  # percentage
         self.now_precipitation = self.now['pcpn']  # millimeter
         self.now_presure = float(self.now['pres']) / 10  # kPa
-        AlertObj.Info("Finish analyzing weather data")
+        AlertObj.Info(
+            "Finish analyzing weather data", color=Alert.FOREGROUND_WHITE)
 
 
 def shutdown():
     '''shut down'''
-    AlertObj.Info("Finish all in {} s".format(time.time() - runtime))
+    AlertObj.Info(
+        "Finish all in {} s".format(time.time() - runtime),
+        color=Alert.FOREGROUND_WHITE)
     os.system('pause')
     exit()
 
@@ -114,11 +120,12 @@ with request.urlopen(ip_url) as urlf:
     ipstring = ip.decode()
     if ipstring == '':
         AlertObj.Warn("Fail to get your IPv4 - Address")
-# For Local IP(Might not be used actually...)
 hostname = socket.gethostname()
-AlertObj.Info("Hello %s user." % hostname)
+AlertObj.Info("Hello %s user." % hostname, color=Alert.FOREGROUND_WHITE)
 AlertObj.Info("Your IPv4 Address:    {}".format(ipstring))
+
 now = time.time()
+
 url = "https://www.ipip.net/ip.html"
 string = request.urlopen(url).read()
 xre = b'(<div style="text-align: center;color:red;font-size: 20px;font-weight: 600;">)(.+)(</div>)'
@@ -126,9 +133,10 @@ z = re.search(xre, string).group(2).decode()
 location = z.split(" ")
 city = location[-1]
 # finish
-key = ''  # TODO: Get a key for yourself!
+key = ''  # BUY A KEY BY YOURSELF
 if key == '':
-    print("No available key.")
+    print('No available key')
+    exit()
 url = "https://free-api.heweather.com/v5/weather?city=" + quote(
     city) + "&key=" + key
 
@@ -136,6 +144,7 @@ content = request.urlopen(url).read()
 data = json.loads(content.decode())
 
 wobj = WeatherInfo(data)
+tmpColor = Alert.FOREGROUND_WHITE
 if float(wobj.latitude) >= 0:
     lat = 'N'
 else:
@@ -144,27 +153,84 @@ if float(wobj.longitude) >= 0:
     lon = 'E'
 else:
     lon = 'W'
-AlertObj.Info("==============================")
+AlertObj.Info(
+    "============================================================",
+    color=Alert.FOREGROUND_WHITE)
 AlertObj.Info("Weather Forecast for {}".format(city))
-AlertObj.Info("latitude:      {}°{}\n\t         longitude:     {}°{}".format(
-    wobj.latitude, lat, wobj.longitude, lon))
+AlertObj.Info(
+    "latitude:      {}°{}\n\t\t longitude:     {}°{}".format(
+        wobj.latitude, lat, wobj.longitude, lon),
+    color=Alert.FOREGROUND_WHITE)
+if int(wobj.now_temperature) >= 33 or int(wobj.now_feel) >= 33:
+    tempColor = Alert.FOREGROUND_RED
+elif 25 <= int(wobj.now_temperature) < 33 and 25 <= int(wobj.now_feel) < 33:
+    tempColor = Alert.FOREGROUND_YELLOW
+elif 16 < int(wobj.now_temperature) < 25 and 16 < int(wobj.now_feel) < 25:
+    tempColor = Alert.FOREGROUND_GREEN
+else:
+    pass
 AlertObj.Info(
     "Real Time Weather: \n\t\t Condition: {}    Temperature: {}℃\n\t\t Sendible Temperature: {}℃".
-    format(wobj.now_cond, wobj.now_temperature, wobj.now_feel))
-AlertObj.Info("Update at {} (local)\n\t\t\t   {} (utc)".format(
-    wobj.updatetime_loc, wobj.updatetime_utc))
+    format(wobj.now_cond, wobj.now_temperature, wobj.now_feel),
+    color=tempColor)
+if int(wobj.today_temperature_min) >= 33:
+    tempColor = Alert.FOREGROUND_RED
+elif 25 <= int(wobj.today_temperature_min) < 33:
+    tempColor = Alert.FOREGROUND_YELLOW
+elif 16 <= int(wobj.today_temperature_min) < 25:
+    tempColor = Alert.FOREGROUND_GREEN
+else:
+    tempColor = Alert.FOREGROUND_BLUE
+AlertObj.Info(
+    "Minimum Temperature: {}".format(wobj.today_temperature_min),
+    color=tempColor)
+if int(wobj.today_temperature_max) >= 33:
+    tempColor = Alert.FOREGROUND_RED
+elif 25 <= int(wobj.today_temperature_max) < 33:
+    tempColor = Alert.FOREGROUND_YELLOW
+elif 16 <= int(wobj.today_temperature_max) < 25:
+    tempColor = Alert.FOREGROUND_GREEN
+else:
+    tempColor = Alert.FOREGROUND_BLUE
+AlertObj.Info(
+    "Maximum Temperature: {}".format(wobj.today_temperature_max),
+    color=tempColor)
+AlertObj.Info(
+    "Update at {} (local)\n\t\t\t   {} (utc)".format(wobj.updatetime_loc,
+                                                     wobj.updatetime_utc),
+    color=Alert.FOREGROUND_WHITE)
+try:
+    if int(wobj.aqi) <= 50:
+        tempColor = Alert.FOREGROUND_BLUE
+    elif 50 < int(wobj.aqi) <= 100:
+        tempColor = Alert.FOREGROUND_GREEN
+    elif 100 < int(wobj.aqi) <= 150:
+        tempColor = Alert.FOREGROUND_YELLOW
+    elif 150 < int(wobj.aqi) <= 200:
+        tempColor = Alert.FOREGROUND_RED
+    else:
+        tempColor = Alert.FOREGROUND_BLACK | Alert.BACKGROUND_DARKRED
+except:
+    pass
 try:
     AlertObj.Info(
-        "Real time AQI: \n\t         Total AQI: {}    pm10: {}    pm2.5 {}\n\t         AirQuality: {}".
+        "Real time AQI: \n\t\t Total AQI: {}    pm10: {}    pm2.5 {}\n\t\t AirQuality: {}".
         format(wobj.aqi, wobj.pm10, wobj.pm25, wobj.qlty))
 except:
     try:
-        AlertObj.Info("lack some Air Quality data.")
         AlertObj.Info(
-            "Real time AQI: \n\t         Total AQI: {}    pm10: {}    pm2.5: {}".
-            format(wobj.aqi, wobj.pm10, wobj.pm25))
+            "lack some Air Quality data.", color=Alert.FOREGROUND_YELLOW)
+        AlertObj.Info(
+            "Real time AQI: \n\t\t Total AQI: {}    pm10: {}    pm2.5: {}".
+            format(wobj.aqi, wobj.pm10, wobj.pm25),
+            color=tempColor)
     except:
-        AlertObj.Warn("No avaiable Air Quality data.")
+        AlertObj.Warn(
+            "No avaiable Air Quality data.", color=Alert.BACKGROUND_RED)
 
-AlertObj.Info("==============================")
+AlertObj.Info(
+    "============================================================",
+    color=Alert.FOREGROUND_WHITE)
 shutdown()
+AlertObj.Info("Finish all in {} s".format(time.time() - runtime))
+os.system('pause')
